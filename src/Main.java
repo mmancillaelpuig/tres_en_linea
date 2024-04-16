0f8bd0997045f34fd56724dee75f38db1f402a6c
dam1 <mmancilla@elpuig.xeill.net>
2024-04-16 17:46:15 +0200

    Missatge inicial amb opcions.

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Benvingut al tres en línia! Escull una de les següents opcions per a continuar:\n1.Nova Partida\n2.Carregar Partida" +
+                "\n3.Configuració\n4.Sortir");
+
+        int start = sc.nextInt();
+
     }
 }
