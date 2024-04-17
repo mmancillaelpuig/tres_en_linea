d998338253d6f8907fb95dfdfe23c15366cea78c
DavidBarbosaOlayo <dbarbosa@elpuig.xeill.net>
2024-04-17 19:36:28 +0200

    Refactoring clase

--- src/Main.java
+++ src/Main.java
@@ -3,40 +3,7 @@ import  java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        boolean exit = false;
 
-        while (!exit) {
-            System.out.println("Benvingut al tres en línia! Escull una de les següents opcions per a continuar:\n1.Nova Partida\n2.Carregar Partida" +
-                    "\n3.Configuració\n4.Sortir");
-
-            int start = sc.nextInt();
-            switch (start){
-                case 1:
-                    System.out.println("Iniciant partida...");
-                    exit = true;
-                    break;
-                case 2:
-                    System.out.println("Carregant partida...");
-                    exit = true;
-                    break;
-                case 3:
-                    System.out.println("Accedint a la configuració...");
-                    exit = true;
-                    break;
-                case 4:
-                    System.out.println("Sortint...");
-                    exit = true;
-                    sc.close();
-                    break;
-                default:
-                    System.out.println("No es una opció correcta, torna de nou!");
-                    break;
-            }
-
-        }
-
-
-
-
+        TUI.mostrarMenu(sc);
     }
 }
---
+++ src/TUI.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class TUI {
+    public static void mostrarMenu(Scanner sc) {
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("Benvingut al tres en línia! Escull una de les següents opcions per a continuar:\n1.Nova Partida\n2.Carregar Partida" +
+                    "\n3.Configuració\n4.Sortir");
+
+            int start = sc.nextInt();
+            switch (start) {
+                case 1:
+                    System.out.println("Iniciant partida...");
+                    exit = true;
+                    break;
+                case 2:
+                    System.out.println("Carregant partida...");
+                    exit = true;
+                    break;
+                case 3:
+                    System.out.println("Accedint a la configuració...");
+                    exit = true;
+                    break;
+                case 4:
+                    System.out.println("Sortint...");
+                    exit = true;
+                    sc.close();
+                    break;
+                default:
+                    System.out.println("No es una opció correcta, torna de nou!");
+                    break;
+            }
+
+        }
+    }
+    public void iniciarPartida(Scanner sc){
+
+
+    }
+}
