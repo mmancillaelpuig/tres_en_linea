084088f666aafc26bffff0402e9c2ff83d83eb62
dam1 <mmancilla@elpuig.xeill.net>
2024-04-24 19:27:55 +0200

    Pruebas metodos

--- src/provesJoc.java
+++ src/provesJoc.java
@@ -1,3 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+
 public class provesJoc {
+
+    @org.junit.jupiter.api.Test
+    void NovaPartida(){
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void Jugar(){
+
+
+    }
+
+
 
 }
