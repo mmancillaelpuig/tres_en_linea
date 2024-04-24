4b5d7a1bfa950f0187ab8308ae88cc97456bfdeb
DavidBarbosaOlayo <dbarbosa@elpuig.xeill.net>
2024-04-24 20:06:38 +0200

    Test implementado, falta lógica de las pruebas.

--- src/Joc.java
+++ src/Joc.java
@@ -1,10 +1,15 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Joc {
+
+    public Joc() {
+    }
+
     public void novaPartida(){
         throw new NotImplementedException();
     }
     public void jugar(){
+
         throw new NotImplementedException();
     }
     public void jugadaGuanyadora(){
---
+++ src/JocTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JocTest {
+
+    @Test
+    void novaPartida_jugador() {
+        Joc j = new Joc();
+        j.novaPartida();
+
+
+    }
+
+    @Test
+    void novaPartida_taulell() {
+        Joc j = new Joc();
+        j.novaPartida();
+
+    }
+
+    @Test
+    void jugar() {
+    }
+}
