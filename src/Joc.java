02d0a333faaabe94d6db4f714eea9574e7c98c65
DavidBarbosaOlayo <dbarbosa@elpuig.xeill.net>
2024-04-24 19:26:47 +0200

    clase Joc implementado

--- src/Joc.java
+++ src/Joc.java
@@ -1,2 +1,13 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class Joc {
+    public void novaPartida(){
+        throw new NotImplementedException();
+    }
+    public void jugar(){
+        throw new NotImplementedException();
+    }
+    public void jugadaGuanyadora(){
+        throw new NotImplementedException();
+    }
 }
