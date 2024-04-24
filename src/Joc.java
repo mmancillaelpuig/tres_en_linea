c97f1738f3d9f591b0588dda26ce330ff7e147b0
dam1 <mmancilla@elpuig.xeill.net>
2024-04-24 19:09:55 +0200

    - Funcionalitat Main / TUI - Classe provesJoc

---
+++ src/Joc.java
@@ -0,0 +1,2 @@
+public class Joc {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,47 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import  java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         TUI tui = new TUI();
-        tui.mostrarMenu(sc);
+        tui.mostrarMenu();
+
+        int opcio = sc.nextInt();
+        switch (opcio) {
+            case 1:
+                novaPartida();
+                break;
+                case 2:
+                    carregarPartida();
+                    break;
+                case 3:
+                    configuracio();
+                    break;
+                case 4:
+                    sortir();
+                    break;
+                default:
+                    System.out.println("No es una opció correcta, torna de nou!");
+                    tui.mostrarMenu();
+            }
+
+    }
+
+    public static void novaPartida(){
+        throw new NotImplementedException();
+    }
+
+    public static void carregarPartida(){
+        throw new NotImplementedException();
+    }
+
+    public static void configuracio(){
+        throw new NotImplementedException();
+    }
+
+    public static void sortir(){
+        System.exit(0);
     }
 }
